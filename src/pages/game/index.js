9abf78cddc7e1081956487e40e3d727e54a03641
Rodolpho Caetano <rphfti@hotmail.com>
2019-01-29 22:49:25 +0000

    Aula 23

---
+++ src/pages/game/index.js
@@ -0,0 +1,9 @@
+const $memoryCardFront = document.createElement("article");
+createMemoryCard();
+createMemoryCard();
+createMemoryCardFront();
+createMemoryCardFront();
+createMemoryCardFront();
+createMemoryCard();
+createMemoryCard();
+createMemoryCard();
