de338e05c59a128b5e1dd83e6aa2543fa475e639
Rodolpho Caetano <rphfti@hotmail.com>
2019-02-05 19:39:52 +0000

    Colocando o CSS dentro do JS Desafio único

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,4 +1,4 @@
-const createMemoryCard = () => ` 
+const createMemoryCard = ({ src, nameClass, alt }) => ` 
   <article class = "memory-card ${nameClass}">
     <img
     class="icon"
@@ -7,6 +7,45 @@ const createMemoryCard = () => `
     onclick="handleClick()">
   </article>
 `;
+const $head = document.querySelector("head");
+const $style = document.createElement("style");
+$style.textContent = `
+.memory-card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 145px;
+  height: 145px;
+  background-color: #f25a70;
+  border-radius: 30px;
+  box-shadow: 0 3px 6px 0 #00000029;
+  position: relative;
+  margin: 5px;
+  cursor: pointer;
+}
+.memory-card.-front {
+  background-color: #fff;
+}
+.memory-card.-front::before {
+  position: absolute;
+  content: "";
+  width: 95px;
+  height: 95px;
+  background-color: #d4d4d4;
+  border-radius: 50%;
+}
+.memory-card > .icon {
+  width: 100px;
+  height: 100px;
+}
+.memory-card.-front > .icon {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  transform: translateY(-12px);
+}
+`;
+$head.insertBefore($style, null);
 
 const handleClick = () => console.log("ae");
 
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,39 +1,39 @@
 const $root = document.querySelector("#root");
 
 const $cardsWrapper = createCardsWrapper();
-const $memoryCard = createMemoryCard(
-  (nameClass = ""),
-  (alt = "Gueio mascote CollabCode"),
-  (src = "img/icon-collabcode.png")
-);
-const $memoryCardBug = createMemoryCard(
-  (alt = "Icone de Bug"),
-  (src = "img/icon-bug.png"),
-  (nameClass = "-front")
-);
-const $memoryCardJS = createMemoryCard(
-  (nameClass = "-front"),
-  (src = "img/icon-js.png"),
-  (alt = "Icone JS")
-);
-const $memoryCardC = createMemoryCard(
-  (nameClass = "-front"),
-  (alt = "Icone do C"),
-  (src = "img/icon-c.png")
-);
-const $memoryCardPHP = createMemoryCard(
-  (nameClass = "-front"),
-  (alt = "Icone do PHP"),
-  (src = "img/icon-php.png")
-);
+const $memoryCard = createMemoryCard({
+  nameClass: "",
+  alt: "Gueio mascote CollabCode",
+  src: "img/icon-collabcode.png"
+});
+const $memoryCardBug = createMemoryCard({
+  alt: "Icone de Bug",
+  src: "img/icon-bug.png",
+  nameClass: "-front"
+});
+const $memoryCardJS = createMemoryCard({
+  nameClass: "-front",
+  src: "img/icon-js.png",
+  alt: "Icone JavaScript"
+});
+const $memoryCardC = createMemoryCard({
+  nameClass: "-front",
+  alt: "Icone do C++",
+  src: "img/icon-c.png"
+});
+const $memoryCardPHP = createMemoryCard({
+  nameClass: "-front",
+  alt: "Icone do PHP",
+  src: "img/icon-php.png"
+});
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBug);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardPHP);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBug);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBug);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardPHP);
 
 $root.insertAdjacentElement("beforeend", $cardsWrapper);
 
