7bac1aad70f00da9708e33b2bdcf5925a78265a5
Rodolpho <rphfti@hotmail.com>
2019-05-13 19:46:41 -0300

    Criado o object eye-collabcode

---
+++ src/objects/eye-collabcode/index.js
@@ -0,0 +1,12 @@
+const eyeCollabcode = (function() {
+  const module = {};
+
+  module._style = () => {};
+
+  module.render = () => `<label>Mostrar senha</label>
+  `;
+
+  return {
+    render: module.render
+  };
+})();
